2de2537498913f4b70baf962627c2f423f023559
Steve Edson <steve.edson@jaywing.com>
2015-11-03 16:37:35 +0000

    install ansible

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,4 @@
 which brew > /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+which ansible-playbook > /dev/null || brew install ansible
 
 ansible-playbook -i "localhost," -c local -v playbook.yml
