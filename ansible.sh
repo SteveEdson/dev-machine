57406e2b18ce53abfd524365aeba0c9cfc934adb
Steve Edson <steve.edson@jaywing.com>
2015-11-03 17:04:47 +0000

    updated tasks

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,8 @@
+# Install Homebrew, if not already installed
 which brew > /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Ansible, if not already installed
 which ansible-playbook > /dev/null || brew install ansible
 
+# Provision machine with ansible
 ansible-playbook -i "localhost," -c local -v playbook.yml
