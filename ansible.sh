66287fdbdea9c51a4cae2f7bdee4cdfcd8f2d6ba
Steve Edson <steve.edson@jaywing.com>
2016-08-23 09:16:23 +0100

    sudo changes

--- ansible.sh
+++ ansible.sh
@@ -5,4 +5,4 @@ which brew > /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.co
 which ansible-playbook > /dev/null || brew install ansible
 
 # Provision machine with ansible
-ansible-playbook -i "localhost," -c local --ask-become-pass playbook.yml
+sudo ansible-playbook -i "localhost," -c local --become-method=su playbook.yml
